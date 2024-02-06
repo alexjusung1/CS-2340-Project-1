bc41874859da9e883c23997a3f275eeb32beed61
SleevedCables <53129009+SleevedCables@users.noreply.github.com>
2024-02-06 14:19:10 -0500

    Fixed Wording and implemented select ViewModel methods

--- app/src/main/java/com/example/cs2340project1/ui/classes/ClassesViewModel.java
+++ app/src/main/java/com/example/cs2340project1/ui/classes/ClassesViewModel.java
@@ -9,6 +9,7 @@ import com.example.cs2340project1.data.ClassData;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class ClassesViewModel extends ViewModel {
     private final MutableLiveData<List<ClassData>> classList;
@@ -29,6 +30,39 @@ public class ClassesViewModel extends ViewModel {
 
     public void attachClassListObserver(LifecycleOwner owner, Observer<List<ClassData>> observer) {
         classList.observe(owner, observer);
+    }
+
+    public void addClass(ClassData data) {
+        List<ClassData> list = classList.getValue();
+        list.add(data);
+        classList.setValue(list);
+    }
+
+    public void removeClass(ClassData data) {
+        List<ClassData> list = classList.getValue();
+        list.remove(data);
+        classList.setValue(list);
+    }
+
+    /**
+     *  Overload that removes class by class name (not used - just added in case)
+     * @param className name of class to remove
+     * @return classData object that was removed
+     */
+    public ClassData removeClass(String className) {
+        List<ClassData> list = classList.getValue();
+        ClassData ret = null;
+        for (ClassData data : list) {
+            if (data.getClassName().equals(className)) {
+                ret = data;
+            }
+        }
+        if (ret == null) {
+            throw new NoSuchElementException("No class with such class name in list");
+        }
+        list.remove(ret);
+        classList.setValue(list);
+        return ret;
     }
 
     // TODO: Implement adding to classList when implementing ClassesFragment
